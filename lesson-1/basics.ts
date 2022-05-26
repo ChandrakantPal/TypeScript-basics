8d3edb5f2e1553940a4448b24afdb438478cb224
ChandrakantPal <sanjay161097@gmail.com>
2022-05-27 00:14:47 +0530

    comments

--- lesson-1/basics.ts
+++ lesson-1/basics.ts
@@ -1,13 +1,19 @@
+// string
 let userName: string = 'Jack'
+
+// boolean
 let hasLoggedIn: boolean = true
 
 userName += ' Herrington'
 
 console.log(userName)
 
+// number
 let num: number = 10
 
+// RegEx
 let Regex: RegExp = /foo/
 
+// Arrays
 const names: string[] = userName.split(' ')
 const numArr: Array<number> = [1, 2, 3]
