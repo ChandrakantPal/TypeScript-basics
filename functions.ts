672b559806c9098773e9c208edaccaed2f1cc418
ChandrakantPal <sanjay161097@gmail.com>
2022-05-28 00:42:20 +0530

    addStrings

--- functions.ts
+++ functions.ts
@@ -1,5 +1,7 @@
-function addNumbers(a: number, b: number) {
+function addNumbers(a: number, b: number): number {
   return a + b
 }
 
 export default addNumbers
+
+export const addStrings = (str1: string, str2: string) => `${str1} ${str2}`
