4361576e3485d72006388c3c511cc2874ddbe165
ChandrakantPal <sanjay161097@gmail.com>
2022-05-28 01:19:59 +0530

    comments added

--- functions.ts
+++ functions.ts
@@ -4,23 +4,30 @@ function addNumbers(a: number, b: number): number {
 
 export default addNumbers
 
+// defaulting a parameter
 export const addStrings = (str1: string, str2: string = ''): string =>
   `${str1} ${str2}`
 
+// union type
 export const format = (title: string, param: string | number): string =>
   `${title} ${param}`
 
+// viod return type
 export const printFormat = (title: string, param: string | number): void => {
   console.log(format(title, param))
 }
 
+// Promise param type
 export const fetchData = (url: string): Promise<string> =>
   Promise.resolve(`Data from ${url}`)
 
-function introduce(salutation: string, ...names: string[]): string {
+// rest param type
+export function introduce(salutation: string, ...names: string[]): string {
   return `${salutation} ${names.join(' ')}`
 }
 
+// testing type enforcement at compile time
 export function getName(user: { first: string; last: string }): string {
+  // ?. to avoid error during runtime
   return `${user?.first} ${user?.last}`
 }
