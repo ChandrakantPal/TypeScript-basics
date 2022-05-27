09a5823d4745458804028b62c723c698887bde9a
ChandrakantPal <sanjay161097@gmail.com>
2022-05-28 00:54:17 +0530

    format union type

--- functions.ts
+++ functions.ts
@@ -6,3 +6,6 @@ export default addNumbers
 
 export const addStrings = (str1: string, str2: string = ''): string =>
   `${str1} ${str2}`
+
+export const format = (title: string, param: string | number): string =>
+  `${title} ${param}`
