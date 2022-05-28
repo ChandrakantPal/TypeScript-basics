03abbe0d0a3ef782d557354db38af31355702dfa
ChandrakantPal <sanjay161097@gmail.com>
2022-05-29 00:34:36 +0530

    extend keyword used

--- challenge-1.ts
+++ challenge-1.ts
@@ -4,22 +4,15 @@ interface House {
   planets: string | string[]
 }
 
-interface HouseWithID {
+interface HouseWithID extends House {
   id: number
-  name: string
-  planets: string | string[]
 }
 
-function findHouses(houses: string): HouseWithID[]
-function findHouses(
-  houses: string,
-  filter: (house: House) => boolean
-): HouseWithID[]
-function findHouses(houses: House[]): HouseWithID[]
-function findHouses(
-  houses: House[],
-  filter: (house: House) => boolean
-): HouseWithID[]
+// function findHouses(houses: string | House[]): HouseWithID[]
+// function findHouses(
+//   houses: string | House[],
+//   filter: (house: House) => boolean
+// ): HouseWithID[]
 function findHouses(
   input: string | House[],
   filter?: (house: House) => boolean
