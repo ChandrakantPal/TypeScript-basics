d29db272588b730d42fc1afb4606551dddb6fb72
ChandrakantPal <sanjay161097@gmail.com>
2022-05-28 21:07:04 +0530

    callback function with parameters and return type

--- funcs-and-funcs.ts
+++ funcs-and-funcs.ts
@@ -3,3 +3,11 @@ export function printToFile(text: string, callback: () => void): void {
   console.log(text)
   callback()
 }
+
+// callback function with parameters and return type
+export function arrayMutate(
+  numbers: number[],
+  mutate: (v: number) => number
+): number[] {
+  return numbers.map(mutate)
+}
