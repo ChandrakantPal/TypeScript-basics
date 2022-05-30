11eaa4795ff5fac0f3171883f63ec1acc5335c1b
ChandrakantPal <sanjay161097@gmail.com>
2022-05-30 15:42:13 +0530

    printing optional

--- optionals.ts
+++ optionals.ts
@@ -1,6 +1,6 @@
 // Optional Parameters
 function printIngredient(quantity: string, ingredient: string, extra?: string) {
-  console.log(`${quantity} ${ingredient} ${extra}`)
+  console.log(`${quantity} ${ingredient} ${extra ? `${extra}` : ''}`)
 }
 
 printIngredient('1C', 'Flour')
