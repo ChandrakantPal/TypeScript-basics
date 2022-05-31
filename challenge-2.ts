56cbcf71ca0994cccfdf376071a9b91e2c102898
ChandrakantPal <sanjay161097@gmail.com>
2022-05-31 18:07:00 +0530

    myFilter

--- challenge-2.ts
+++ challenge-2.ts
@@ -6,3 +6,9 @@ function myForEach<T>(items: T[], forEachFunc: (v: T) => void): void {
 }
 
 myForEach(['a', 'b', 'c'], (v) => console.log(`forEach ${v}`))
+
+function myFilter<T>(items: T[], filterFunc: (v: T) => boolean): T[] {
+  return items.reduce((a: T[], v: T) => (filterFunc(v) ? [...a, v] : a), [])
+}
+
+console.log(myFilter([1, 2, 3, 4, 5, 6, 7, 8], (v) => v % 2 === 0))
