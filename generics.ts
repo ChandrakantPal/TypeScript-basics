d395caa5aa78a01c15946f1aa89ee97153bca060
ChandrakantPal <sanjay161097@gmail.com>
2022-05-31 15:55:32 +0530

    Overriding inferred generic type

--- generics.ts
+++ generics.ts
@@ -12,3 +12,11 @@ const [st1getter, st1setter] = simpleState(1)
 console.log(st1getter())
 st1setter(2)
 console.log(st1getter())
+
+// Overriding inferred generic type
+const [st2getter, st2setter] = simpleState<string | null>(null)
+console.log(st2getter())
+st2setter('str')
+console.log(st2getter())
+st2setter(null)
+console.log(st2getter())
