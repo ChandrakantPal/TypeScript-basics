902d05b8ec021dbc6b2e52e38eb13f9b520dd8b4
ChandrakantPal <sanjay161097@gmail.com>
2022-06-01 15:15:33 +0530

    numeric literal

--- enums.ts
+++ enums.ts
@@ -2,6 +2,7 @@
 // const loading = 'loading'
 // const loaded = 'loaded'
 
+// enums
 enum LoadingState {
   beforeLoad = 'beforeLoad',
   loading = 'loading',
@@ -15,3 +16,15 @@ const englishLoadingStates = {
 const isLoading = (state: LoadingState) => state === LoadingState.loading
 
 console.log(isLoading(LoadingState.beforeLoad))
+
+// Literal types
+
+function rollDice(dice: 1 | 2 | 3): number {
+  let pip = 0
+  for (let i = 0; i < dice; i++) {
+    pip += Math.floor(Math.random() * 5) + 1
+  }
+  return pip
+}
+
+console.log(rollDice(3))
