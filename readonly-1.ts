c836184be4f8c8f3eeba13dd00733769a1d61c4f
ChandrakantPal <sanjay161097@gmail.com>
2022-06-01 14:51:39 +0530

    makeCoordinate

--- readonly-1.ts
+++ readonly-1.ts
@@ -13,3 +13,14 @@ function makeCat(name: string, breed: string): Readonly<Cat> {
 
 const usul = makeCat('Usul', 'Tabby')
 // usul.name = "CAT" throws error cause fields are readonly
+
+function makeCoordinate(
+  x: number,
+  y: number,
+  z: number
+): readonly [number, number, number] {
+  return [x, y, z]
+}
+
+const c1 = makeCoordinate(10, 20, 30)
+// c1[0] = 50 readonly error will be thrown
