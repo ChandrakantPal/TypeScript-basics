4bed268b9d8abea0b4f7b80f2a9ba1a0ec677151
ChandrakantPal <sanjay161097@gmail.com>
2022-06-02 16:55:04 +0530

    Mapped types

--- mapped.ts
+++ mapped.ts
@@ -8,3 +8,15 @@ const dog: MyFlexibleDogInfo = {
   breed: 'Mutt',
   age: 22,
 }
+
+// Mapped type
+interface DogInfo {
+  name: string
+  age: number
+}
+
+type OptionsFlags<Type> = {
+  [Property in keyof Type]: null
+}
+
+type DogInfoOptions = OptionsFlags<DogInfo>
