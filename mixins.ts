0d3bfee7170be9bea00a6ed6b3dc470a2993b18e
ChandrakantPal <sanjay161097@gmail.com>
2022-06-03 01:03:56 +0530

    createLoggerClass

--- mixins.ts
+++ mixins.ts
@@ -6,3 +6,16 @@ function myLogFunction() {
 
 const logger = myLogFunction()
 logger('your string')
+
+function createLoggerClass() {
+  return class MyLoggerClass {
+    private completeLog: string = ''
+    log(str: string) {
+      console.log(str)
+      this.completeLog += str + '\n'
+    }
+    dumpLog() {
+      return this.completeLog
+    }
+  }
+}
