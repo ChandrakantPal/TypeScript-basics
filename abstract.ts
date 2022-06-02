e4748d1bf1a9ea9a33f64741bcee1b3bf9fd3f79
ChandrakantPal <sanjay161097@gmail.com>
2022-06-03 00:03:45 +0530

    Ryu extends StreetFighter

--- abstract.ts
+++ abstract.ts
@@ -6,3 +6,11 @@ abstract class StreetFighter {
 
   abstract getSpecialAttack(): string
 }
+
+// const ryu = new StreetFighter() Cannot create an instance of an abstract class.
+
+class Ryu extends StreetFighter {
+  getSpecialAttack(): string {
+    return 'Hadoken'
+  }
+}
