c0b4ec1e6a533426ba3e45ebfb3979813886c91d
ChandrakantPal <sanjay161097@gmail.com>
2022-06-02 23:38:53 +0530

    static instance static addDog

--- readonly-2.ts
+++ readonly-2.ts
@@ -1,3 +1,4 @@
+// readonly
 class Doggy {
   constructor(public readonly name: string, public readonly age: number) {}
 }
@@ -5,3 +6,20 @@ class Doggy {
 const lgg = new Doggy('LG', 13)
 // lgg.name = "Foo" Cannot assign to 'name' because it is a read-only property.
 console.log(lgg.name)
+
+// statics
+class DogList {
+  private doggies: Doggy[] = []
+
+  static instance: DogList = new DogList()
+
+  private constructor() {}
+
+  static addDog(dog: Doggy) {
+    DogList.instance.doggies.push(dog)
+  }
+}
+
+DogList.instance
+
+// const dl = new DogList()  Constructor of class 'DogList' is private and only accessible within the class declaration.
