4932abd5d453ba44b04a8b4a42a2f39f9449ea63
ChandrakantPal <sanjay161097@gmail.com>
2022-06-02 23:32:57 +0530

    readonly

--- readonly-2.ts
+++ readonly-2.ts
@@ -1,6 +1,7 @@
 class Doggy {
-  constructor(public name: string, public age: number) {}
+  constructor(public readonly name: string, public readonly age: number) {}
 }
 
 const lgg = new Doggy('LG', 13)
+// lgg.name = "Foo" Cannot assign to 'name' because it is a read-only property.
 console.log(lgg.name)
