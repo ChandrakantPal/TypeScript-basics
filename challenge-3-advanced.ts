27275f85288a0e6c03b25c5b73757531c3f6f1e8
ChandrakantPal <sanjay161097@gmail.com>
2022-06-04 15:15:51 +0530

    types issue fixed

---
+++ challenge-3-advanced.ts
@@ -0,0 +1,99 @@
+type Handler<T> = {
+  [Prop in keyof T as `map${Capitalize<string & Prop>}`]?: (
+    data: T[Prop]
+  ) => T[Prop]
+} & {
+  [Prop in keyof T as `filter${Capitalize<string & Prop>}`]?: (
+    data: T[Prop]
+  ) => boolean
+}
+
+type ProcessedEvent<T> = {
+  eventName: keyof T
+  data: T[keyof T]
+}
+
+class EventProcessor<T extends {}> {
+  private handlers: Handler<T>[] = []
+  private processed: ProcessedEvent<T>[] = []
+
+  handleEvent<K extends keyof T>(eventName: K, data: T[K]): void {
+    let allowEvent = true
+    const capitalize = (s: string) =>
+      `${s.charAt(0).toLocaleUpperCase}${s.slice(1)}`
+    for (const handler of this.handlers) {
+      const filterFunc = handler[
+        `filter${capitalize(eventName as string)}` as keyof Handler<T>
+      ] as unknown as ((value: T[K]) => boolean) | undefined
+      if (filterFunc && !filterFunc(data)) {
+        allowEvent = false
+        break
+      }
+    }
+    if (allowEvent) {
+      let mappedData = { ...data }
+      for (const handler of this.handlers) {
+        const mapFunc = handler[
+          `map${capitalize(eventName as string)}` as keyof Handler<T>
+        ] as unknown as ((value: T[K]) => T[K]) | undefined
+
+        if (mapFunc) {
+          mappedData = <T[K]>mapFunc(mappedData)
+        }
+      }
+      this.processed.push({
+        eventName,
+        data: mappedData,
+      })
+    }
+  }
+
+  addHandler(handler: Handler<T>): void {
+    this.handlers.push(handler)
+  }
+
+  getProcessedEvents() {
+    return this.processed
+  }
+}
+
+interface EventMap {
+  login: { user?: string; name?: string; hasSession?: boolean }
+  logout: { user?: string }
+}
+
+class UserEventProcessor extends EventProcessor<EventMap> {}
+
+const uep = new UserEventProcessor()
+
+uep.addHandler({
+  filterLogin: ({ user }) => Boolean(user),
+  mapLogin: (data) => ({
+    ...data,
+    hasSession: Boolean(data.user && data.name),
+  }),
+})
+
+uep.handleEvent('login', {
+  name: 'jack',
+})
+uep.handleEvent('login', {
+  user: 'tom',
+  name: 'tomas',
+})
+uep.handleEvent('logout', {
+  user: 'tom',
+})
+
+console.log(uep.getProcessedEvents())
+
+/*
+  Result:
+  [
+    {
+      eventName: 'login',
+      data: { user: 'tom', name: 'tomas', hasSession: true }
+    },
+    { eventName: 'logout', data: { user: 'tom' } }
+  ]
+  */
