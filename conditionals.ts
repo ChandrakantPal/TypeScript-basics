301c077ac7e3ed2be317bc48eb7f13ec986f92f7
ChandrakantPal <sanjay161097@gmail.com>
2022-06-05 15:29:02 +0530

    fetching pokemon

--- conditionals.ts
+++ conditionals.ts
@@ -11,19 +11,24 @@ interface PokemonResults {
 
 // conditionals
 type FetchPokemonResult<T> = T extends undefined
-  ? Promise<PokemonResults[]>
+  ? Promise<PokemonResults>
   : void
 
-function fetchPokemon<T extends undefined | ((data: PokemonResults[]) => void)>(
+function fetchPokemon<T extends undefined | ((data: PokemonResults) => void)>(
   url: string,
   cb?: T
 ): FetchPokemonResult<T> {
   if (cb) {
     fetch(url)
       .then((resp) => resp.json())
-      .then(cb)
+      .then((data) => cb(data as PokemonResults))
     return undefined as FetchPokemonResult<T>
   } else {
     return fetch(url).then((resp) => resp.json()) as FetchPokemonResult<T>
   }
 }
+
+fetchPokemon('https://pokeapi.co/api/v2/pokemon?limit=10', (data) => {
+  data.results.forEach(({ name }) => console.log(name))
+  //   console.log(data)
+})
