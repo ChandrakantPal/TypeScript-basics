adec47f693bb5b12a1c285bb3d1ae568409c1892
ChandrakantPal <sanjay161097@gmail.com>
2022-06-06 17:32:48 +0530

    addFullName

--- utility-types-2.ts
+++ utility-types-2.ts
@@ -2,3 +2,10 @@ type Name = {
   first: string
   last: string
 }
+
+function addFullName(name: Name): Name & { fullName: string } {
+  return {
+    ...name,
+    fullName: `${name.first} ${name.last}`,
+  }
+}
