79efd1ccf91e2626b6f68891309135223f4b3056
ChandrakantPal <sanjay161097@gmail.com>
2022-06-07 16:43:20 +0530

    Heading

--- episode-20/src/App.tsx
+++ episode-20/src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css'
 
+const Heading = () => <h2>Heading</h2>
+
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Heading />
+    </div>
+  )
 }
 
 export default App
