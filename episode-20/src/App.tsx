f3252be528ddbc415974a9dc79a0563301277af0
ChandrakantPal <sanjay161097@gmail.com>
2022-06-07 16:47:41 +0530

    title prop

--- episode-20/src/App.tsx
+++ episode-20/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
 
-const Heading = () => <h2>Heading</h2>
+const Heading = ({ title }: { title: string }) => <h2>{title}</h2>
 
 function App() {
   return (
     <div>
-      <Heading />
+      <Heading title="Introduction" />
     </div>
   )
 }
