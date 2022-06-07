4aa16ecea5ce554997e84495a410354d63eaa930
ChandrakantPal <sanjay161097@gmail.com>
2022-06-07 18:10:21 +0530

    list component

--- episode-20/src/App.tsx
+++ episode-20/src/App.tsx
@@ -9,11 +9,20 @@ const Box: React.FunctionComponent<React.PropsWithChildren> = ({
   children,
 }) => <div style={{ padding: '1rem', fontWeight: 'bold' }}>{children}</div>
 
+const List: React.FC<{ items: string[] }> = ({ items }) => (
+  <ul>
+    {items.map((item, index) => (
+      <li key={`${index}-${item}`}>{item}</li>
+    ))}
+  </ul>
+)
+
 function App() {
   return (
     <div>
       <Heading title="Introduction" />
       <Box>Hello there</Box>
+      <List items={['one', 'two', 'three']} />
     </div>
   )
 }
