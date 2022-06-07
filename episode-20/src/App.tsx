8f009d9aa364dbc77a2a5dade52d3291aa62aa47
ChandrakantPal <sanjay161097@gmail.com>
2022-06-07 17:25:34 +0530

    box compnent

--- episode-20/src/App.tsx
+++ episode-20/src/App.tsx
@@ -1,11 +1,19 @@
+import * as React from 'react'
 import './App.css'
 
-const Heading = ({ title }: { title: string }) => <h2>{title}</h2>
+const Heading: React.FunctionComponent<{ title: string }> = ({ title }) => (
+  <h2>{title}</h2>
+)
+
+const Box: React.FunctionComponent<React.PropsWithChildren> = ({
+  children,
+}) => <div style={{ padding: '1rem', fontWeight: 'bold' }}>{children}</div>
 
 function App() {
   return (
     <div>
       <Heading title="Introduction" />
+      <Box>Hello there</Box>
     </div>
   )
 }
