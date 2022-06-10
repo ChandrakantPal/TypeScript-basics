a4e7b709e1ecbcb470d793876758c3915f7b12a9
ChandrakantPal <sanjay161097@gmail.com>
2022-06-10 22:12:03 +0530

    itemClick prop

--- episode-20/src/App.tsx
+++ episode-20/src/App.tsx
@@ -37,12 +37,14 @@ const Button: React.FunctionComponent<
 function UL<T>({
   items,
   render,
+  itemClick,
 }: React.DetailedHTMLProps<
   React.HTMLAttributes<HTMLUListElement>,
   HTMLUListElement
 > & {
   items: T[]
   render: (item: T) => React.ReactNode
+  itemClick: (item: T) => void
 }) {
   return (
     <ul>
