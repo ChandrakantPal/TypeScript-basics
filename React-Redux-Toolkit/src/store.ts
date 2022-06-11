ad3a698a033206242828dd19c8ca7e9513f274a2
ChandrakantPal <sanjay161097@gmail.com>
2022-06-12 02:07:57 +0530

    todosSlice

--- React-Redux-Toolkit/src/store.ts
+++ React-Redux-Toolkit/src/store.ts
@@ -1,10 +1,49 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface Todo {
+  id: number
+  done: boolean
+  text: string
+}
+
+interface TodoSliceState {
+  todos: Todo[]
+}
+
+const initialState: TodoSliceState = {
+  todos: [],
+}
+
+export const todosSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      state.todos = [
+        ...state.todos,
+        {
+          id: state.todos.length,
+          text: action.payload,
+          done: false,
+        },
+      ]
+    },
+    removeTodo: (state, action: PayloadAction<number>) => {
+      state.todos = state.todos.filter(({ id }) => id !== action.payload)
+    },
+  },
+})
+
+export const { addTodo, removeTodo } = todosSlice.actions
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    todos: todosSlice.reducer,
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export const selectTodos = (state: RootState) => state.todos.todos
+// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+// export type AppDispatch = typeof store.dispatch
