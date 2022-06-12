d976310f174145575b0e231d073a680371128b07
ChandrakantPal <sanjay161097@gmail.com>
2022-06-12 17:42:15 +0530

    useTodos refactored to use xState

--- React-xState/src/useTodos.ts
+++ React-xState/src/useTodos.ts
@@ -1,5 +1,6 @@
+import { useMachine } from '@xstate/react'
 import * as React from 'react'
-import { createGlobalState } from 'react-use'
+import { assign, createMachine } from 'xstate'
 
 interface Todo {
   id: number
@@ -7,37 +8,73 @@ interface Todo {
   text: string
 }
 
-const useGlobalTodos = createGlobalState<Todo[]>([])
+const todoMachine = createMachine<
+  { todos: Todo[] },
+  | { type: 'SET_TODOS'; todos: Todo[] }
+  | { type: 'ADD_TODO'; text: string }
+  | { type: 'REMOVE_TODO'; removeId: number }
+>({
+  id: 'todoMachine',
+  initial: 'editing',
+  context: {
+    todos: [],
+  },
+  states: {
+    editing: {
+      on: {
+        ADD_TODO: {
+          actions: assign({
+            todos: ({ todos }, { text }) => [
+              ...todos,
+              {
+                id: todos.length,
+                text,
+                done: false,
+              },
+            ],
+          }),
+        },
+        REMOVE_TODO: {
+          actions: assign({
+            todos: ({ todos }, { removeId }) =>
+              todos.filter(({ id }) => id !== removeId),
+          }),
+        },
+        SET_TODOS: {
+          actions: assign({
+            todos: (_, { todos }) => todos,
+          }),
+        },
+      },
+    },
+    working: {},
+  },
+})
 
 export function useTodos(initialTodos: Todo[]): {
   todos: Todo[]
   addTodo: (text: string) => void
   removeTodo: (id: number) => void
 } {
-  const [todos, setTodos] = useGlobalTodos()
-
-  React.useEffect(() => {
-    setTodos(initialTodos)
-  }, [initialTodos])
+  const [state, send] = useMachine(todoMachine)
 
   const addTodo = React.useCallback(
     (text: string) => {
-      setTodos([
-        ...todos,
-        {
-          id: todos.length,
-          text: text,
-          done: false,
-        },
-      ])
+      send({
+        type: 'ADD_TODO',
+        text,
+      })
     },
-    [todos]
+    [send]
   )
   const removeTodo = React.useCallback(
     (removeId: number) => {
-      setTodos(todos.filter(({ id }) => id !== removeId))
+      send({
+        type: 'REMOVE_TODO',
+        removeId,
+      })
     },
-    [todos]
+    [send]
   )
-  return { todos, addTodo, removeTodo }
+  return { todos: state.context.todos, addTodo, removeTodo }
 }
