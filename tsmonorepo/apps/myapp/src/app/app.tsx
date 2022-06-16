11b1a0c5abebcf1ab28c438324048faa838ac25d
ChandrakantPal <sanjay161097@gmail.com>
2022-06-17 00:53:32 +0530

    carousel used

--- tsmonorepo/apps/myapp/src/app/app.tsx
+++ tsmonorepo/apps/myapp/src/app/app.tsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
-
+import { Carousel } from '@tsmonorepo/mycomps';
 export function App() {
   return (
     <>
       <NxWelcome title="myapp" />
+      <Carousel />
       <div />
     </>
   );
