3632b6c61d932a4cb2e7b72843b1923e36fa9317
ChandrakantPal <sanjay161097@gmail.com>
2022-06-17 01:00:42 +0530

    checking for live update

--- tsmonorepo/libs/mycomps/src/lib/carousel/carousel.tsx
+++ tsmonorepo/libs/mycomps/src/lib/carousel/carousel.tsx
@@ -6,7 +6,7 @@ export interface CarouselProps {}
 export function Carousel(props: CarouselProps) {
   return (
     <div className={styles['container']}>
-      <h1>Hi! From My Carousel!</h1>
+      <h1>Hi! From My Carousel! - Live update??</h1>
     </div>
   );
 }
