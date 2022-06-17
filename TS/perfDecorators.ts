e931d3571f58d7f8f9c54c554676ebd096d99424
ChandrakantPal <sanjay161097@gmail.com>
2022-06-18 02:03:20 +0530

    casting timings

--- TS/perfDecorators.ts
+++ TS/perfDecorators.ts
@@ -19,7 +19,14 @@ export function timing() {
       const start = performance.now()
       const out = await value.apply(this, args)
       const end = performance.now()
-      console.log(end - start)
+      if ((this as { __timings: unknown[] }).__timings) {
+        ;(this as { __timings: unknown[] }).__timings.push({
+          method: propertyKey,
+          time: end - start,
+        })
+      } else {
+        console.log(end - start)
+      }
       return out
     }
   }
