62ec45f25853469713f7018d2ef0e26bbeac8f74
ChandrakantPal <sanjay161097@gmail.com>
2022-06-19 03:38:24 +0530

    redux counter test

---
+++ ts-react-testing/src/ReduxCounter.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { ReduxCounter } from './ReduxCounter'
+import { Provider } from 'react-redux'
+import { createStore } from './store'
+
+test('increment', () => {
+  render(
+    <Provider store={createStore()}>
+      <ReduxCounter />
+    </Provider>
+  )
+  const counter = screen.getByRole('contentinfo')
+  expect(counter).toHaveTextContent('0')
+
+  const addButton = screen.getByText(/Increment/i)
+  fireEvent.click(addButton)
+  expect(counter).toHaveTextContent('1')
+})
+
+test('increment again', () => {
+  render(
+    <Provider store={createStore()}>
+      <ReduxCounter />
+    </Provider>
+  )
+  const counter = screen.getByRole('contentinfo')
+  expect(counter).toHaveTextContent('0')
+
+  const addButton = screen.getByText(/Increment/i)
+  fireEvent.click(addButton)
+  expect(counter).toHaveTextContent('1')
+})
