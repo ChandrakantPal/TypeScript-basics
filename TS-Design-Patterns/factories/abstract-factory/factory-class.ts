3fa379d4f53550b44556cc04ae6a4e6dbd94926a
ChandrakantPal <sanjay161097@gmail.com>
2022-06-22 22:22:56 +0530

    ProductionLogger

--- TS-Design-Patterns/factories/abstract-factory/factory-class.ts
+++ TS-Design-Patterns/factories/abstract-factory/factory-class.ts
@@ -4,3 +4,10 @@ interface ILogger {
   debug(message: string): void
   error(message: string): void
 }
+
+class ProductionLogger implements ILogger {
+  info(message: string): void {}
+  warn(message: string): void {}
+  debug(message: string): void {}
+  error(message: string): void {}
+}
