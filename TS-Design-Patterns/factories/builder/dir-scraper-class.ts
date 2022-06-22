eff26c0889e82882ceb8a7f41cd79cecef0be575
ChandrakantPal <sanjay161097@gmail.com>
2022-06-23 01:06:05 +0530

    directoryScraper

--- TS-Design-Patterns/factories/builder/dir-scraper-class.ts
+++ TS-Design-Patterns/factories/builder/dir-scraper-class.ts
@@ -1,5 +1,23 @@
+import fs from 'fs'
 interface IFileReader {
   isJSONFile(file: string): boolean
   readText(file: string): string
   readJson(file: string): unknown
+}
+
+class DirectoryScraper {
+  constructor(public dirPath: string, public fileReader: IFileReader) {}
+
+  scanFiles() {
+    return fs
+      .readdirSync(this.dirPath)
+      .reduce<Record<string, unknown>>((acc, file) => {
+        if (this.fileReader.isJSONFile(file)) {
+          acc[file] = this.fileReader.readJson(`${this.dirPath}/${file}`)
+        } else {
+          acc[file] = this.fileReader.readText(`${this.dirPath}/${file}`)
+        }
+        return acc
+      }, {})
+  }
 }
