a07f3755907c8bae8fc57ee5de889bb2d8f97b13
ChandrakantPal <sanjay161097@gmail.com>
2022-06-22 23:04:04 +0530

    lower case

--- TS-Design-Patterns/factories/abstract-factory/factory-class-test.ts
+++ TS-Design-Patterns/factories/abstract-factory/factory-class-test.ts
@@ -2,7 +2,7 @@ import { LoggerFactory } from './factory-class'
 
 const logger = LoggerFactory.createLogger()
 
-logger.debug('Debug message')
+logger.debug('debug message')
 logger.warn('warn message')
 logger.info('info message')
 logger.error('error message')
