007db50042b53ab42aefce7a8b69f8e95231d9ee
ChandrakantPal <sanjay161097@gmail.com>
2022-06-24 02:45:21 +0530

    logging the output

--- TS-Design-Patterns/handlers/index.ts
+++ TS-Design-Patterns/handlers/index.ts
@@ -1,3 +1,5 @@
+import fs from 'fs'
+
 export function createHandlerStack<MessageType>() {
   const subscribers: Set<(msg: MessageType) => undefined | unknown> = new Set()
   return {
@@ -18,4 +20,15 @@ export function createHandlerStack<MessageType>() {
       return data
     },
   }
+}
+
+const handlers = createHandlerStack<{
+  name: string
+  contents: string
+}>()
+
+for (const name of fs.readdirSync('./files')) {
+  const contents = fs.readFileSync(`./files/${name}`, 'utf-8')
+  const output = handlers.publish({ name, contents })
+  console.log(`${name}: ${JSON.stringify(output)}`)
 }
