a836e19c655e81107a0ff0ac53cd48e7d0c5e43d
ChandrakantPal <sanjay161097@gmail.com>
2022-06-24 01:34:07 +0530

    publish

--- TS-Design-Patterns/pup-sub/subscribable-class.ts
+++ TS-Design-Patterns/pup-sub/subscribable-class.ts
@@ -1,8 +1,15 @@
-export class Subscribable {
-  private subscribers: Set<() => void> = new Set()
+export class Subscribable<MessageType> {
+  private subscribers: Set<(msg: MessageType) => void> = new Set()
   constructor() {}
 
-  subscribe(cb: () => void) {
+  subscribe(cb: (msg: MessageType) => void): () => void {
     this.subscribers.add(cb)
+    return () => {
+      this.subscribers.delete(cb)
+    }
+  }
+
+  publish(msg: MessageType): void {
+    this.subscribers.forEach((cb) => cb(msg))
   }
 }
