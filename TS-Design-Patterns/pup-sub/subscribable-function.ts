52279e5259fbea44afc4ac6dfcc1d5e0d30b1afb
ChandrakantPal <sanjay161097@gmail.com>
2022-06-24 01:51:30 +0530

    functional subscribable

---
+++ TS-Design-Patterns/pup-sub/subscribable-function.ts
@@ -0,0 +1,14 @@
+export function createSubscribable<MessageType>() {
+  const subscribers: Set<(msg: MessageType) => void> = new Set()
+  return {
+    subscribe(cb: (msg: MessageType) => void): () => void {
+      subscribers.add(cb)
+      return () => {
+        subscribers.delete(cb)
+      }
+    },
+    publish(msg: MessageType): void {
+      subscribers.forEach((cb) => cb(msg))
+    },
+  }
+}
