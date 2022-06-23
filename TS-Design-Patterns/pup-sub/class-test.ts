fadc68a8056b942e52155feba7dcd8c9b3d7ab8a
ChandrakantPal <sanjay161097@gmail.com>
2022-06-24 01:40:01 +0530

    unsub

--- TS-Design-Patterns/pup-sub/class-test.ts
+++ TS-Design-Patterns/pup-sub/class-test.ts
@@ -1,7 +1,8 @@
 import { Subscribable } from './subscribable-class'
 
 const sub = new Subscribable<string>()
-sub.subscribe(console.log)
+const unsub = sub.subscribe(console.log)
 sub.publish('Hello')
 sub.publish('Whatever')
+unsub()
 sub.publish('Goodbye')
