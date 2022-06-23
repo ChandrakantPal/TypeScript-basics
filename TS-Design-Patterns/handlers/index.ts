4b22eca70b3555dde4ebcf7e4c13bd26df19dc1d
ChandrakantPal <sanjay161097@gmail.com>
2022-06-24 02:33:19 +0530

    createHandlerStack

---
+++ TS-Design-Patterns/handlers/index.ts
@@ -0,0 +1,21 @@
+export function createHandlerStack<MessageType>() {
+  const subscribers: Set<(msg: MessageType) => undefined | unknown> = new Set()
+  return {
+    subscribe(cb: (msg: MessageType) => undefined | unknown): () => void {
+      subscribers.add(cb)
+      return () => {
+        subscribers.delete(cb)
+      }
+    },
+    publish(msg: MessageType): undefined | unknown {
+      let data: unknown
+      for (const subscriber of subscribers) {
+        data = subscriber(msg)
+        if (data === undefined) {
+          break
+        }
+      }
+      return data
+    },
+  }
+}
