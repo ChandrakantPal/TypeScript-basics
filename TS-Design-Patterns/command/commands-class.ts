e6dcf964050707f4f79d50029f1f3de6f943594d
ChandrakantPal <sanjay161097@gmail.com>
2022-06-26 00:48:51 +0530

    CommandStack

--- TS-Design-Patterns/command/commands-class.ts
+++ TS-Design-Patterns/command/commands-class.ts
@@ -2,3 +2,12 @@ abstract class Command<State> {
   abstract execute(state: State): State
   abstract undo(state: State): State
 }
+
+class CommandStack<State> {
+  private stack: Command<State>[] = []
+  constructor(private _state: State) {}
+
+  get state() {
+    return this._state
+  }
+}
