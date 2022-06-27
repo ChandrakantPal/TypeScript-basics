660d3a42d25e5b92b984a83fd2e97b6936da8b04
ChandrakantPal <sanjay161097@gmail.com>
2022-06-28 00:12:20 +0530

    counter component

--- TS-Design-Patterns/simple-state-manager/src/App.tsx
+++ TS-Design-Patterns/simple-state-manager/src/App.tsx
@@ -4,6 +4,17 @@ const globalState = createObservable({
   count: 0,
 })
 
+function Counter() {
+  const counter = useObservable(globalState)
+  return (
+    <div>
+      <div>Count = {counter.count}</div>
+
+      <button onClick={() => (counter.count += 1)}>Add One</button>
+    </div>
+  )
+}
+
 function App() {
   return <div className="App"></div>
 }
