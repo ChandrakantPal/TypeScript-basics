909369aba23b06f4a2915f2b6adcedf4f43ab050
ChandrakantPal <sanjay161097@gmail.com>
2022-06-27 23:27:47 +0530

    makeURLFlyweight

---
+++ TS-Design-Patterns/flyweight/index.ts
@@ -0,0 +1,42 @@
+import fetch from 'node-fetch'
+
+interface Pokemon {
+  species: {
+    name: string
+    url: string
+  }
+}
+
+interface PokemonList {
+  count: number
+  next: string
+  previous?: any
+  results: {
+    name: string
+    url: string
+  }[]
+}
+
+function makeURLFlyweight<ReturnType>(url: Record<string, string>) {
+  const myObject: Record<string, Promise<ReturnType>> = {}
+
+  return new Proxy(myObject, {
+    get: (target, name: string) => {
+      console.log(`Fetching ${name} ${url[name]}`)
+    },
+  })
+}
+
+;(async () => {
+  const pokemon = (await (
+    await fetch('https://pokeapi.co/api/v2/pokemon/')
+  ).json()) as PokemonList
+  const urls = pokemon.results.reduce(
+    (acc, { name, url }) => ({
+      ...acc,
+      [name]: url,
+    }),
+    {}
+  )
+  const lookup = makeURLFlyweight<Pokemon>(urls)
+})()
